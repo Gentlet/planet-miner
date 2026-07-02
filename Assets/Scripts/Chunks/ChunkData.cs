0b5b01c3076fd6ae3ccb9cdda14a7628d8cdfc80
Gentlet <cyc07297@gmail.com>
2026-07-02 19:31:16 +0900

    ChunkUtility 파일 분리

--- Assets/Scripts/Chunks/ChunkData.cs
+++ Assets/Scripts/Chunks/ChunkData.cs
@@ -8,42 +8,7 @@ public enum FloorTypeEnum : byte
     PlacedResource
 }
 
-public static class ChunkUtility
-{
-    public const int chunkSize = 16;
-    public const int cellCount = chunkSize * chunkSize;
-
-    public static int2 ToChunkPosition(int2 cell)
-    {
-        return new int2(
-            FloorDiv(cell.x, chunkSize),
-            FloorDiv(cell.y, chunkSize));
-    }
-
-    public static int2 ToLocalCell(int2 cell)
-    {
-        int2 chunkPosition = ToChunkPosition(cell);
-        return cell - chunkPosition * chunkSize;
-    }
-
-    public static int ToCellIndex(int2 localCell)
-    {
-        return localCell.y * chunkSize + localCell.x;
-    }
-
-    private static int FloorDiv(int value, int divisor)
-    {
-        int quotient = value / divisor;
-        int remainder = value % divisor;
-
-        if (remainder != 0 && value < 0)
-            quotient--;
-
-        return quotient;
-    }
-}
-
-public sealed class ChunkCell
+public class ChunkCell
 {
     private readonly List<Entity> _items = new();
 
